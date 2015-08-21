9cbe68d8f3382663c4b8d4e3d78327f65c0a427f
hirondas <hiron_cse@yahoo.com>
2015-08-21 15:58:34 +0000

    done

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var express = require('express');
+var logger = require("./logger");
 var app = express();
 
 /*app.get('/', function(request, response){
     response.sendFile(__dirname + '/public/index.html');
 });*/
 
+app.use(logger);
+
 app.use(express.static("public"));
+
+app.get('/blocks', function(request, response){
+    var blocks = ["Fixed", "Movable", "Rotating"];
+    response.json(blocks);
+})
 
 
 app.listen(process.env.PORT);
---
+++ public/client.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $.get('/blocks', function(blocks){
+        var list = [];
+        for(var i in blocks){
+            list.push($('<li>', {text: blocks[i] }));
+        }
+        $('.block-list').append(list);
+    });
+});
