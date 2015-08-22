0bda027180aa7a05ac5ffd752feb0650b4aa1009
Hiron Das <hcdas.09@gmail.com>
2015-08-23 01:02:04 +0600

    finish of 3rd lesson

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(logger);
 app.use(express.static("public"));
 
 app.get('/blocks', function(request, response){
-    var blocks = ["Fixed", "Movable", "Rotating"];
+    // var blocks = ["Fixed", "Movable", "Rotating"];
 
     if (request.query.limit >= 0) {
 
@@ -19,8 +19,46 @@ app.get('/blocks', function(request, response){
 
     }else {
 
-    	response.json(blocks);
+    	response.json(Object.keys(blocks));
     }
+});
+
+var blocks = {
+	'Fixed': 'Fastened securely in position',
+	'Movable': 'Capable of being moved',
+	'Rotating': 'Moving in a circle around its center'
+};
+
+var location = {
+	'Fixed' : 'First floor',
+	'Movable' : "Second floor",
+	'Rotation' : "Penthouse"
+};
+
+app.param('name', function(request, response, next){
+	var name = request.params.name;
+	var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
+
+	request.blockName = block;
+	next();
+});
+
+app.get('/blocks/:name', function(request, response){
+	var description = blocks[request.blockName];
+	if(!description) {
+		response.status(404).json("No description found for " + request.param.name);
+	} else {
+		response.json(description);
+	}
+});
+
+app.get('/location/:name', function(request, response){
+	var description = location[request.blockName];
+	if(!description) {
+		response.status(404).json("No description found for " + request.param.name);
+	} else {
+		response.json(description);
+	}
 });
 
 
