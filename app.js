a1332369b8194e89ad028ebeed40c441b29f58d2
Hiron Das <hcdas.09@gmail.com>
2015-08-24 02:21:43 +0600

    the building block tutorial is finished

--- app.js
+++ app.js
@@ -1,85 +1,16 @@
 var express = require('express');
-var logger = require("./logger");
 var app = express();
+var logger = require("./logger");
 
-var bodyParser = require("body-parser");
-var parseUrlencoded = bodyParser.urlencoded({extended: false});
-
-
-
-/*app.get('/', function(request, response){
-    response.sendFile(__dirname + '/public/index.html');
-});*/
-
-app.use(logger);
 
 app.use(express.static("public"));
-
-app.get('/blocks', function(request, response){
-    // var blocks = ["Fixed", "Movable", "Rotating"];
-
-    if (request.query.limit >= 0) {
-
-    	response.json(blocks.slice(0, request.query.limit));
-
-    }else {
-
-    	response.json(Object.keys(blocks));
-    }
-});
+app.use(logger);
 
 
+var blocks = require('./routes/blocks');
 
-var blocks = {
-	'Fixed': 'Fastened securely in position',
-	'Movable': 'Capable of being moved',
-	'Rotating': 'Moving in a circle around its center'
-};
 
-var location = {
-	'Fixed' : 'First floor',
-	'Movable' : "Second floor",
-	'Rotation' : "Penthouse"
-};
-
-app.param('name', function(request, response, next){
-	var name = request.params.name;
-	var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
-
-	request.blockName = block;
-	next();
-});
-
-app.get('/blocks/:name', function(request, response){
-	var description = blocks[request.blockName];
-	if(!description) {
-		response.status(404).json("No description found for " + request.param.name);
-	} else {
-		response.json(description);
-	}
-});
-
-app.get('/location/:name', function(request, response){
-	var description = location[request.blockName];
-	if(!description) {
-		response.status(404).json("No description found for " + request.param.name);
-	} else {
-		response.json(description);
-	}
-});
-
-app.post('/blocks', parseUrlencoded, function(request, response){
-    var newBlock = request.body;
-    blocks[newBlock.name] = newBlock.description;
-    
-    response.status(201).json(newBlock.name);
-});
-
-app.delete('/blocks/:name', function(request, response){
-    console.log(blocks[request.blockName]);
-    delete blocks[request.blockName];
-    response.sendStatus(200);
-});
+app.use("/blocks", blocks);
 
 
 app.listen(process.env.PORT|| 3000);
